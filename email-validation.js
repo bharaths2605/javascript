86e1a095f1704e1bd7d498b29d171e5ce4924ef4
bharaths2605 <60768675+bharaths2605@users.noreply.github.com>
2021-06-08 17:00:27 +0530

    Add files via upload

---
+++ email-validation.js
@@ -0,0 +1,33 @@
+function ValidateEmail(inputText)
+{
+var mailformat = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+var y = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+var a = inputText.value;
+
+var b = a.split("");
+
+var c = b.indexOf('@');
+
+var d = b.indexOf('.');
+
+var e = a.slice(c+1,d);
+
+
+console.log();
+
+
+if(inputText.value.match(mailformat))
+{
+alert("You have entered a valid email address!");    //The pop up alert for a valid email address
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");    //The pop up alert for an invalid email address
+document.form1.text1.focus();
+return false;
+}
+}
